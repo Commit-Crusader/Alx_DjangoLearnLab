414abd182cfa77d35f8ff6d1d7851e398ac839fe
Jonathan Lartey <lightlartey2000@gmail.com>
2025-08-03 19:16:07 +0000

    built an api endpoint

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
 
 class Book(models.Model):
-    title = ...
-    author = ...
-    published_date = ...
-
-
+    title = models.CharField(max_length=200, default="Unknown Book")
+    author = models.CharField(max_length=100, default="Unknown Author")
+    published_date = models.DateField(default=timezone.now)  # Use current date as default
+    
+    
     def __str__(self):
-        return ...
+        return self.title
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+class BookList(generics.ListAPIView):
+    """
+    API view to retrieve a list of all books.
+    
+    This view extends ListAPIView which provides GET method handler
+    for listing a queryset of model instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
