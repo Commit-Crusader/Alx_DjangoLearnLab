cfee633c5fd328800a51112fd5d53d1cafbc4dde
Jonathan Lartey <lightlartey2000@gmail.com>
2025-08-11 03:10:32 +0000

    add features to views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def list_books(request):
+    books = book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'relationship_app/list_books.html', context)
