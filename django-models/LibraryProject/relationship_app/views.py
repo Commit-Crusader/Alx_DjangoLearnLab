c7e77748dcb981666a36fad126a1f51a66eb0f5f
Jonathan Lartey <lightlartey2000@gmail.com>
2025-08-11 03:16:42 +0000

    add features to views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Library
+from django.views.generic import DetailView
 # Create your views here.
 
 
@@ -7,3 +8,8 @@ def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    """A class-based view for displaying details of a specific library."""
+  model = Library
+  template_name = 'relationship_app/library_detail.html'
