b1a53d1ee542b9e8244d8b8e5b9cc30fd455a52f
Jonathan Lartey <lightlartey2000@gmail.com>
2025-08-17 01:27:45 +0000

    Setup Django project

--- advanced-api-project/advanced_api_project/api/admin.py
+++ advanced-api-project/advanced_api_project/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author, Book
 
 # Register your models here.
+# Register the Author model with the admin site
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+# Register the Book model with the admin site
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('author',)
+    search_fields = ('title',)
