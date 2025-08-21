5611cd1abd3f3a35fed762a4912bc6115ad95770
Jonathan Lartey <lightlartey2000@gmail.com>
2025-08-21 20:44:15 +0000

    Created the implementation of the static files for login, and register

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog_db',
         'USER': 'postgres',
-        'PASSWORD': 'postgre',
+        'PASSWORD': '0556964210',
         'HOST': 'localhost',
         'PORT': '5432',
     }
