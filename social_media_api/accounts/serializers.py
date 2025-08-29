fc120d4385aa16f96b19673fc8c4e0e6f5c70e14
Jonathan Lartey <lightlartey2000@gmail.com>
2025-08-29 20:03:52 +0000

    Implemented views and serializers for user registration, login, and token retrieval

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -20,7 +20,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # create user
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
